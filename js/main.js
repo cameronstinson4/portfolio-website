e74fd8c81a36ace51637b4bd5ee7509622c36adb
Cameron Stinson <cameronstinson4@gmail.com>
2017-09-19 23:26:01 -0400

    added page navigation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,39 @@
 (function($){
     $(function(){
-  
-      $('.button-collapse').sideNav();
-      $('.parallax').parallax();
-  
+
+        var activeContent = $("#about");
+        var activeNav = $("#aboutButton");
+        
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+
+        var changeContentPage = function(event, button, id) {
+            event.preventDefault();
+
+            activeContent.removeClass("scale-in");
+            activeContent.addClass("nosize scale-out");
+            activeNav.removeClass("active");
+
+            activeContent = $(id);
+            activeNav = $(button);
+            activeNav.addClass("active")
+
+            activeContent.removeClass("nosize scale-out");
+            activeContent.addClass("scale-in");
+        }
+
+        $("#experienceButton").on("click", function(event) {
+            changeContentPage(event, this, "#experience");
+        })        
+        $("#projectsButton").on("click", function(event) {
+            changeContentPage(event, this, "#projects");
+        })        
+        $("#personalButton").on("click", function(event) {
+            changeContentPage(event, this, "#personal");
+        })        
+        $("#aboutButton").on("click", function(event) {
+            changeContentPage(event, this, "#about");
+        })
     }); // end of document ready
-  })(jQuery); // end of jQuery name space
+})(jQuery); // end of jQuery name space
+
