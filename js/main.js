14a8348a7d5a3e12a30f0034825b674cf921bac8
Cameron Stinson <burninater44@gmail.com>
2017-09-21 23:20:07 -0400

    added carousel and footer

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 (function($){
     $(function(){
-
-        var activeContent = $("#about");
         var activeNav = $("#aboutButton");
+        
+        $('.carousel').carousel({fullWidth: true, indicators: false});
         
         $('.button-collapse').sideNav();
         $('.parallax').parallax();
@@ -10,29 +10,26 @@
         var changeContentPage = function(event, button, id) {
             event.preventDefault();
 
-            activeContent.removeClass("scale-in");
-            activeContent.addClass("nosize scale-out");
+            $('.carousel').carousel('set', id);
+            
             activeNav.removeClass("active");
 
-            activeContent = $(id);
             activeNav = $(button);
             activeNav.addClass("active")
 
-            activeContent.removeClass("nosize scale-out");
-            activeContent.addClass("scale-in");
         }
 
         $("#experienceButton").on("click", function(event) {
-            changeContentPage(event, this, "#experience");
+            changeContentPage(event, this, 1);
         })        
         $("#projectsButton").on("click", function(event) {
-            changeContentPage(event, this, "#projects");
+            changeContentPage(event, this, 2);
         })        
         $("#personalButton").on("click", function(event) {
-            changeContentPage(event, this, "#personal");
+            changeContentPage(event, this, 3);
         })        
         $("#aboutButton").on("click", function(event) {
-            changeContentPage(event, this, "#about");
+            changeContentPage(event, this, 4);
         })
     }); // end of document ready
 })(jQuery); // end of jQuery name space
