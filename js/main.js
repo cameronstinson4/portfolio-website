0616dd508323ae4d39ae4b1d07ffb09196697c26
Cameron Stinson <cameronstinson4@gmail.com>
2017-09-26 00:12:16 -0400

    redesigned header and added slide in nav title

--- js/main.js
+++ js/main.js
@@ -1,17 +1,38 @@
-(function($){
-    $(function(){
+(function ($) {
+
+    $(function () {
+
         var activeNav = $("#aboutButton");
-        
-        $('.carousel').carousel({fullWidth: true, indicators: false});
-        
+        var hasScrolledDown = false;
+        var navTitle = $('#scrolledTitle');
+        var isNavTitleHidden = false;
+        const navTitleOffset = navTitle[0].clientWidth;
+
+        function toggleNavTitle() {
+            if (isNavTitleHidden) {
+                //show nav bar title
+                navTitle.animate({ "margin-left": '+=' + navTitleOffset });
+                isNavTitleHidden = false;
+            }
+            else {
+                //hiding nav title
+                navTitle.css({ "margin-left": '-=' + navTitleOffset });
+                isNavTitleHidden = true;
+            }
+        }
+        //hide the nav bar title at the beginining
+        toggleNavTitle();
+
+        $('.carousel').carousel({ fullWidth: true, indicators: false });
+
         $('.button-collapse').sideNav();
         $('.parallax').parallax();
 
-        var changeContentPage = function(event, button, id) {
+        var changeContentPage = function (event, button, id) {
             event.preventDefault();
 
             $('.carousel').carousel('set', id);
-            
+
             activeNav.removeClass("active");
 
             activeNav = $(button);
@@ -19,18 +40,35 @@
 
         }
 
-        $("#experienceButton").on("click", function(event) {
+        $("#experienceButton").on("click", function (event) {
             changeContentPage(event, this, 1);
-        })        
-        $("#projectsButton").on("click", function(event) {
+        })
+        $("#projectsButton").on("click", function (event) {
             changeContentPage(event, this, 2);
-        })        
-        $("#personalButton").on("click", function(event) {
+        })
+        $("#personalButton").on("click", function (event) {
             changeContentPage(event, this, 3);
-        })        
-        $("#aboutButton").on("click", function(event) {
+        })
+        $("#aboutButton").on("click", function (event) {
             changeContentPage(event, this, 4);
         })
+
+        $(window).scroll(function () {
+            if ($(document).scrollTop() > window.innerHeight / 2) {
+                if (isNavTitleHidden) {
+                    //show nav bar title
+                    navTitle.animate({ "margin-left": '+=' + navTitleOffset });
+                    isNavTitleHidden = false;
+                }
+            }
+            else if ($(document).scrollTop() <= window.innerHeight / 2) {
+                if (!isNavTitleHidden) {
+                    //hiding nav title
+                    navTitle.css({ "margin-left": '-=' + navTitleOffset });
+                    isNavTitleHidden = true;
+                }
+            }
+        });
     }); // end of document ready
 })(jQuery); // end of jQuery name space
 
